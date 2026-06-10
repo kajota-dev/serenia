d6781275d5aaa16b65a4520e7c8eab15a82f7d8a
kajota-dev <kajotadev@gmail.com>
2026-06-10 16:00:13 -0600

    pixel de meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '1707516430495117');
+              fbq('init', '1319185906911063');
               fbq('track', 'PageView');
             `,
           }}
@@ -68,12 +69,10 @@ export default function RootLayout({
       </head>
       <body className={`${manrope.variable} ${playfair.variable} font-display antialiased bg-background-light text-luxury-green`}>
         <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1707516430495117&ev=PageView&noscript=1"
-          />
+          <img height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1707516430495117&ev=PageView&noscript=1" />
+          <img height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1319185906911063&ev=PageView&noscript=1" />
         </noscript>
         {children}
       </body>
